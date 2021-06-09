3a45e4fe81d7a98b73f185f419ac90b880f0148f
kshitij <kshitijk3188@gmail.com>
2021-06-10 03:36:23 +0530

    JSON Server

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,11 @@
 import React, { useState, useEffect } from "react";
-import { uuid } from 'uuidv4';
-import './App.css';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import { uuid } from "uuidv4";
+import "./App.css";
 import Header from "./Header";
 import AddContact from "./AddContact";
 import ContactList from "./ContactList";
+import ContactDetail from "./ContactDetail";
 
 function App() {
   const LOCAL_STORAGE_KEY = "contacts";
@@ -18,23 +20,45 @@ function App() {
     const newContactList = contacts.filter((contact) => {
       return contact.id !== id;
     });
-    setContacts(newContactList);
-  }
 
+    setContacts(newContactList);
+  };
 
   useEffect(() => {
-    const retrieveContacts = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY));
-    if(retrieveContacts) setContacts(retrieveContacts);
+    const retriveContacts = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY));
+    if (retriveContacts) setContacts(retriveContacts);
   }, []);
 
   useEffect(() => {
     localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(contacts));
   }, [contacts]);
+
   return (
     <div className="ui container">
-      <Header />
-      <AddContact addContactHandler={addContactHandler}/>
-      <ContactList contacts={contacts} getContactId={removeContactHandler} />
+      <Router>
+        <Header />
+        <Switch>
+          <Route path="/" exact
+             render={(props) => (
+             <ContactList 
+             {...props}
+             contacts={contacts}
+            getContactId={removeContactHandler}
+              />
+            )}
+          />
+          <Route path="/add" 
+            render={(props) => (
+              <AddContact {...props}
+              addContactHandler={addContactHandler} />
+            )}
+          />
+
+          <Route path="/contact/:id" component={ContactDetail} />
+          
+        </Switch>
+      </Router>
+      
     </div>
   );
 }
