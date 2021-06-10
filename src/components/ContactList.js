b4ade5466c4c7f3d4fb7a6a700e47d5a25a22c64
kshitij <kshitijk3188@gmail.com>
2021-06-11 01:17:08 +0530

    Added Search Bar

--- src/components/ContactList.js
+++ src/components/ContactList.js
@@ -1,10 +1,9 @@
-import React from "react";
+import React, {useRef} from "react";
 import { Link } from 'react-router-dom';
 import ContactCard from "./ContactCard";
 
 const ContactList = (props) => {
-  console.log(props);
-
+  const inputEl = useRef("");
   const deleteConactHandler = (id) => {
     props.getContactId(id);
   };
@@ -18,15 +17,26 @@ const ContactList = (props) => {
       />
     );
   });
+
+  const getSearchTerm = () => {
+    props.searchKeyword(inputEl.current.value);
+  };
+
   return (
-    <div class="main">
+    <div className="main">
       <h2>
         Contact List
         <Link to="/add">
           <button className="ui button blue right">Add Contact</button>
         </Link>
       </h2>
-      <div className="ui celled list">{renderContactList}</div>;
+      <div className="ui search">
+        <div className="ui icon input">
+          <input ref={inputEl} type="text" placeholder="Search Contacts" className="prompt" value={ props.term } onChange={getSearchTerm} />
+          <i className="search icon"></i>
+        </div>
+      </div>
+      <div className="ui celled list">{renderContactList}</div>
     </div>
   );
 };
